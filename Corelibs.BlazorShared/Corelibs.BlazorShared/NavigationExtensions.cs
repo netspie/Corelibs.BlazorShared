eb14765c0fd6884e86562652da2d686234418ac5
dariusz.lusnia <dariusz.lusnia@gmail.com>
2023-01-02 13:57:38 +0100

    Add navigation extensions

---
+++ Corelibs.BlazorShared/Corelibs.BlazorShared/NavigationExtensions.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Corelibs.BlazorShared
+{
+    public static class NavigationExtensions
+    {
+        public static void NavigateToBase(this NavigationManager navigation, bool forceLoad = true) =>
+            navigation.NavigateTo(navigation.BaseUri, forceLoad);
+    }
+}
