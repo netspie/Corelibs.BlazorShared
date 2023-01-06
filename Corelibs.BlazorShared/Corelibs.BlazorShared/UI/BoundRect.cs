840f93bbd1893f6bd5ecfc1da10e64bf65ad8001
dariusz.lusnia <dariusz.lusnia@gmail.com>
2023-01-06 03:49:53 +0100

    Add bouind rect js extension

---
+++ Corelibs.BlazorShared/Corelibs.BlazorShared/UI/BoundRect.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.JSInterop;
+
+namespace Corelibs.BlazorShared.UI
+{
+    public class BoundRect
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public double Top { get; set; }
+        public double Right { get; set; }
+        public double Bottom { get; set; }
+        public double Left { get; set; }
+    }
+
+    public static class BoundRectExtensions
+    {
+        public static ValueTask<BoundRect> GetRect(this IJSRuntime jsRuntime, string className)
+        {
+            return jsRuntime.InvokeAsync<BoundRect>("getBoundingClientRectByClass", className);
+        }
+    }
+}
